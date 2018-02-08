ebab2aaf13cea2bd25ea6aae21bf82a74ddd4107
Sanaz <sanaz.jamloo@gmail.com>
2018-02-07 17:03:10 -0800

    Create transform style for the arms

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+//lets will contain the degrees that we want each arm to move.
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+var date = new Date();
+console.log(date);
+
+
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
