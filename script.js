2d1dd65852434d7afdf066749377385a06da4071
Sanaz <sanaz.jamloo@gmail.com>
2018-02-07 17:07:59 -0800

    Add date variable

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let secPosition = 267;
 
 var date = new Date();
 console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + "Minute: " + min + " Second: " + sec);
 
 
 
